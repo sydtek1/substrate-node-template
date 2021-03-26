867436cd06402f56d021e92530b657bbbbf9c26d
“Assune <dontrail.cotlage@gmail.com>
2021-03-26 00:48:42 -0400

    Added Ink Smart Contract API to runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -350,6 +350,8 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
 		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		
+		// Smart contract Pallet
 		Contracts: pallet_contracts::{Module, Call, Config<T>, Storage, Event<T>},
 	}
 );
